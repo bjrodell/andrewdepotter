47a1fd051813ecd1c51b8730374759d9f210dd1c
bjrodell <bjrodell@gmail.com>
2018-07-25 19:28:13 -0400

    initial commit of site template as is. will work as i go on this project

--- js/closeDropDown.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-function closeDropDown()
-		
-			{				
-			  document.getElementById("divisions").classList.toggle("visiblelinks");
-			  document.getElementById("divisions").classList.toggle("hiddenlinks");
-			}
--- js/dropDown.js
+++ js/dropDown.js
@@ -4,10 +4,6 @@ function dropDown()
 			{	
 			  "use strict";	
 			
-			  document.getElementById("divisions").classList.toggle("visiblelinks");
-			  document.getElementById("divisions").classList.toggle("hiddenlinks");
-			  document.getElementById("navlineone").classList.toggle("blurry");
-			  document.getElementById("navlinetwo").classList.toggle("blurry");
-			  document.getElementById("navlinethree").classList.toggle("blurry");
-			  document.getElementById("navlinefour").classList.toggle("blurry");
+			  document.getElementById("navlist").classList.toggle("visible");
+			  document.getElementById("navlist").classList.toggle("hidden");
 			}
