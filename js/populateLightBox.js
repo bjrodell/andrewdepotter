7a48da9dcf9e54b1ecb933b172872b9333173127
bjrodell <39781176+bjrodell@users.noreply.github.com>
2018-08-04 18:24:30 -0400

    lightbox update - working beta

--- js/populateLightBox.js
+++ js/populateLightBox.js
@@ -1,22 +1,100 @@
 // JavaScript Document
+var gal = document.getElementsByClassName('gallery');
+var thumbnails = document.getElementsByClassName('thumbnail');
+var counter = 0;
+var x = gal.length;
+
 
 jQuery(document).ready(function($){
 	"use strict";
+	
+
+	
+	
+	
 	$('.card-body').click(function(e){
 	  e.preventDefault();
 	  var image_src = $(this).children(".gallery").attr("src"); 
 	  var title = $(this).children(".gallery").attr("title"); 
-	    $('.lightbox').html('<img id="centerStage" src ="' +image_src+ '"  style="height: 85%;width: auto;position: center;">'+'<p>'+ title +'</p>'+$('.lightbox-footer').html()+'<button type="button" class="close" aria-label="Close" style="position: absolute; top: 5px; right: 30px;">'+'<span onclick="closeLightBox()" aria-hidden="true">&times;</span>'+'</button>');
+	  counter = $(this).children(".gallery").attr("alt");
+	  var footerhtml = $('.lightbox-footer').html();
+	    $('.lightbox').html('<img id="centerStage" src ="' +image_src+ '"  style="height: 85%;width: auto;">'+'<p>'+ title +'</p>'+ footerhtml +'<button type="button" class="close" aria-label="Close" style="position: absolute; top: 5px; right: 30px;">'+'<span onclick="closeLightBox()" aria-hidden="true">&times;</span>'+'</button>');
 		$('.lightbox').fadeIn(1000);
 		$('body').css('overflow','hidden');
 		
+		$('.thumbnail').on("click",function(){
+	  
+		counter = $(this).addClass('thumb-border').attr("alt");
+		document.getElementById('centerStage').src = gal[(Math.abs(counter))%x].src;
+		
+		$('.thumbnail').each(function() {
+		if($(this).attr("alt")!= Math.abs(counter)){
+		$(this).removeClass('thumb-border');
+		}
+		});
+		
+		});	
+		
+		$('.but').on("click",function(){
+	  
+		counter = $(this).addClass('thumb-border').attr("alt");
+		document.getElementById('centerStage').src = gal[(Math.abs(counter))%x].src;
+		
+		$('.thumbnail').each(function() {
+		if($(this).attr("alt")!= Math.abs(counter)){
+		$(this).removeClass('thumb-border');
+		}
+		});
+		
+		});
+		
+		
+		
+		
+		
+		
+		
+		
 	});
+	
+/**	function borderJumper(){
+	
+		$('.thumbnail').each(function() {
+			if($(this).attr("alt")!= Math.abs(counter)){
+				$(this).removeClass('thumb-border');
+			}else{
+				$(this).addClass('thumb-border');
+			}
+		}
+	
+		if($('.thumbnail').attr("alt") == Math.abs(counter)){
+			$('.thumbnail').addClass('.thumb-border');
+		}else{
+			$('.thumbnail').removeClass('.thumb-border');
+		} 
+	}
+	
+		*/
+	
+
+	
 });
+	
+function borderJumper(){
+	
+	
+		$('.thumbnail').each(function() {
+		if($(this).attr("alt")!= (Math.abs(counter)%x)){
+		$(this).removeClass('thumb-border');
+		}else{ $(this).addClass('thumb-border') }
+		});
+	
+		if($('.thumbnail').attr("alt") == (Math.abs(counter)%x)){
+			$('.thumbnail').addClass('.thumb-border');
+		}else{
+			$('.thumbnail').removeClass('.thumb-border');
+		} ;
+	}
 
-var gal = document.getElementsByClassName('gallery');
-var counter = 0;
-var x = gal.length;
-
-// below is test shit - don't put below this line unless comfortable with iit getting deleted.
 
 
