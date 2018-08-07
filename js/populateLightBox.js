7f4b0fcdca9986ba7b6e9dcf2f5a86b5a56ed2fa
bjrodell <39781176+bjrodell@users.noreply.github.com>
2018-08-06 22:11:17 -0400

    Lightbox improvements

--- js/populateLightBox.js
+++ js/populateLightBox.js
@@ -15,10 +15,10 @@ jQuery(document).ready(function($){
 	$('.card-body').click(function(e){
 	  e.preventDefault();
 	  var image_src = $(this).children(".gallery").attr("src"); 
-	  var title = $(this).children(".gallery").attr("title"); 
+	  var title = $(this).children(".gallery").attr("title");	//can be used in lightbox to add a blurb of the art  
 	  counter = $(this).children(".gallery").attr("alt");
 	  var footerhtml = $('.lightbox-footer').html();
-	    $('.lightbox').html('<img id="centerStage" src ="' +image_src+ '"  style="height: 85%;width: auto;">'+'<p>'+ title +'</p>'+ footerhtml +'<button type="button" class="close" aria-label="Close" style="position: absolute; top: 5px; right: 30px;">'+'<span onclick="closeLightBox()" aria-hidden="true">&times;</span>'+'</button>');
+	    $('.lightbox').html('<img id="centerStage" src ="' +image_src+ '"  style="height: 95%;width: auto;">'+''+ footerhtml +'<button type="button" class="close" aria-label="Close" style="position: absolute; top: 5px; right: 30px;">'+'<span onclick="closeLightBox()" aria-hidden="true">&times;</span>'+'</button>');
 		$('.lightbox').fadeIn(1000);
 		$('body').css('overflow','hidden');
 		
@@ -57,6 +57,23 @@ function borderJumper(){
 			$('.thumbnail').removeClass('.thumb-border');
 		} ;
 	}
+	
+function nextSlide(){
+	//next slide
+	counter++;
+	document.getElementById('centerStage').src = gal[(Math.abs(counter))%x].src;
+	borderJumper()
+}
 
+function prevSlide(){
+	//previous slide
+	counter--;
+	document.getElementById('centerStage').src = gal[(Math.abs(counter))%x].src;
+	borderJumper();
+	
+	if(counter < 0){
+		counter = (x*10) + 1;
+	}
+}
 
 
