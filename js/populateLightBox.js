c99e40b0c9b669a1048b61eddc3db31038022c97
bjrodell <bjrodell@gmail.com>
2018-08-11 07:46:17 -0400

    commit made to address navbar behavior, dropdowns in nav

--- js/populateLightBox.js
+++ js/populateLightBox.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 var gal = document.getElementsByClassName('gallery');
 var thumbnails = document.getElementsByClassName('thumbnail');
-var counter;
+var counter = 0;
 var x = gal.length;
 
 
@@ -15,16 +15,15 @@ jQuery(document).ready(function($){
 	$('.card-body').click(function(e){
 	  e.preventDefault();
 	  var image_src = $(this).children(".gallery").attr("src"); 
-	  var title = $(this).children(".gallery").attr("title");	//can be used in lightbox to add a blurb of the art  
+	  var title = $(this).children(".gallery").attr("title"); 
 	  counter = $(this).children(".gallery").attr("alt");
-	  borderJumper();
 	  var footerhtml = $('.lightbox-footer').html();
-	    $('.lightbox').html('<img id="centerStage" src ="' +image_src+ '"  style="height: 95%;width: auto;">'+''+ footerhtml +'<button type="button" class="close" aria-label="Close" style="position: absolute; top: 5px; right: 30px;">'+'<span onclick="closeLightBox()" aria-hidden="true">&times;</span>'+'</button>');
+	    $('.lightbox').html('<img id="centerStage" src ="' +image_src+ '"  style="height: 85%;width: auto;">'+'<p>'+ title +'</p>'+ footerhtml +'<button type="button" class="close" aria-label="Close" style="position: absolute; top: 5px; right: 30px;">'+'<span onclick="closeLightBox()" aria-hidden="true">&times;</span>'+'</button>');
 		$('.lightbox').fadeIn(1000);
 		$('body').css('overflow','hidden');
 		
 		$('.thumbnail').on("click",function(){
-	
+	  
 		counter = $(this).addClass('thumb-border').attr("alt");
 		document.getElementById('centerStage').src = gal[(Math.abs(counter))%x].src;
 		
@@ -52,29 +51,12 @@ function borderJumper(){
 		}else{ $(this).addClass('thumb-border') }
 		});
 	
-	//	if($('.thumbnail').attr("alt") == (Math.abs(counter)%x)){
-	//		$('.thumbnail').addClass('.thumb-border');
-	//	}else{
-	//		$('.thumbnail').removeClass('.thumb-border');
-	//	} ;
+		if($('.thumbnail').attr("alt") == (Math.abs(counter)%x)){
+			$('.thumbnail').addClass('.thumb-border');
+		}else{
+			$('.thumbnail').removeClass('.thumb-border');
+		} ;
 	}
-	
-function nextSlide(){
-	//next slide
-	counter++;
-	document.getElementById('centerStage').src = gal[(Math.abs(counter))%x].src;
-	borderJumper();
-}
 
-function prevSlide(){
-	//previous slide
-	counter--;
-	document.getElementById('centerStage').src = gal[(Math.abs(counter))%x].src;
-	borderJumper();
-	
-	if(counter <= 0){
-		counter = (x*10);
-	}
-}
 
 
